46c2f2d49ee2cd41756959a0f50300d8e5af5668
thaisonvnn11 <thaisonvnn11@gmail.com>
2016-05-21 20:50:50 +0700

    [SonDTT] design layout

--- Source_code/QuanLyRapChieuPhim/UserInterFace/UserAuthentication.Designer.cs
+++ Source_code/QuanLyRapChieuPhim/UserInterFace/UserAuthentication.Designer.cs
@@ -28,11 +28,124 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+						| System.Windows.Forms.AnchorStyles.Left)
+						| System.Windows.Forms.AnchorStyles.Right)));
+			this.groupBox1.Controls.Add(this.button2);
+			this.groupBox1.Controls.Add(this.button1);
+			this.groupBox1.Controls.Add(this.textBox2);
+			this.groupBox1.Controls.Add(this.textBox1);
+			this.groupBox1.Controls.Add(this.label2);
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.groupBox1.Location = new System.Drawing.Point(28, 12);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(343, 216);
+			this.groupBox1.TabIndex = 0;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Đăng nhập";
+			this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+			this.label1.Location = new System.Drawing.Point(30, 54);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(112, 16);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Tên đăng nhập";
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+			this.label2.Location = new System.Drawing.Point(30, 96);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(70, 16);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Mật khẩu";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(149, 45);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(164, 29);
+			this.textBox1.TabIndex = 2;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(149, 87);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(164, 29);
+			this.textBox2.TabIndex = 3;
+			// 
+			// button1
+			// 
+			this.button1.CausesValidation = false;
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.ForeColor = System.Drawing.Color.Red;
+			this.button1.Location = new System.Drawing.Point(63, 163);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(97, 34);
+			this.button1.TabIndex = 4;
+			this.button1.Text = "Đăng nhập";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.CausesValidation = false;
+			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button2.ForeColor = System.Drawing.Color.Red;
+			this.button2.Location = new System.Drawing.Point(182, 163);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(97, 34);
+			this.button2.TabIndex = 5;
+			this.button2.Text = "Thoát";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// CUserAuthentication
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "UserAuthentication";
+			this.ClientSize = new System.Drawing.Size(399, 262);
+			this.ControlBox = false;
+			this.Controls.Add(this.groupBox1);
+			this.Name = "CUserAuthentication";
+			this.ShowIcon = false;
+			this.ShowInTaskbar = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.TopMost = true;
+			this.Load += new System.EventHandler(this.CUserAuthentication_Load);
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label2;
 	}
 }
--- Source_code/QuanLyRapChieuPhim/UserInterFace/UserAuthentication.cs
+++ Source_code/QuanLyRapChieuPhim/UserInterFace/UserAuthentication.cs
@@ -40,5 +40,20 @@ namespace QuanLyRapChieuPhim
 		{
 			InitializeComponent();
 		}
+
+		private void CUserAuthentication_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void groupBox1_Enter(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
 	}
 }
