e101135cf17e453bed3fb83b78384eb97ab4078b
thaisonvnn11 <thaisonvnn11@gmail.com>
2016-05-23 20:59:03 +0700

    [SonDTT] Create login controller

--- Source_code/QuanLyRapChieuPhim/Define/Define.cs
+++ Source_code/QuanLyRapChieuPhim/Define/Define.cs
@@ -1,12 +1,21 @@
 ﻿namespace QuanLyRapChieuPhim
 {
-	enum AccessRight
+	/* Quyền đăng nhập trong hệ thống */
+	enum eACESS_RIGHT
 	{
-		eVisitor = 1,
-		eAdministrators,
-		eCashier,
-		eSupervisor,
+		eACESS_DENIED = 0,		/* Không có quyền truy cập */
+		eVISITOR,				/* Người viếng thăm */
+		eADMINISTRATOR,			/* Quản trị viên */
+		eCASHIER,				/* ... */
+		eSUPERVISOR,
 	};
+
+	/* Mã lỗi trả về khi đăng nhập */
+	enum eLOGIN_ERROR
+	{
+		eERROR_OK = 1,			/* Đăng nhập thành công */
+		eERROR_NOK,				/* Đăng không thành công */
+	}
 
 	struct NhanVien
 	{
